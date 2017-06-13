a46b96ebd588a4512dff47e2f7165834d62f6789
mfrancis95 <mikefrancis95@gmail.com>
2017-06-12 20:12:23 -0400

    Prompt for password or username-password pair if omitted from config

--- webhook-puller.js
+++ webhook-puller.js
@@ -1,9 +1,26 @@
 const config = require(process.argv[2] || "./config.json");
+const readlineSync = require("readline-sync");
 const exec = require("child_process").exec;
 const app = require("express")();
 
 if (!config.branch) {
     config.branch = "master";
+}
+
+if (!config.credentials) {
+    config.credentials = {
+        username: readlineSync.question("Enter your GitHub username: "),
+        password: readlineSync.question("Enter your GitHub password: ", {
+            hideEchoBack: true,
+            mask: ""
+        })
+    };
+}
+else if (!config.credentials.password) {
+    config.credentials.password = readlineSync.question("Enter your GitHub password: ", {
+        hideEchoBack: true,
+        mask: ""
+    });
 }
 
 if (config.secret) {
@@ -14,21 +31,21 @@ if (config.secret) {
 
 app.post(config.route || "/", (request, response) => {
     if (!config.secret || (request.isXHub && request.isXHubValid())) {
-        var child = exec(`git pull https://${config.credentials.username}:${config.credentials.password}@github.com/${config.remoteRepo.username}/${config.remoteRepo.name}.git ${config.branch}`, {
+        const child = exec(`git pull https://${config.credentials.username}:${config.credentials.password}@github.com/${config.remoteRepo.username}/${config.remoteRepo.name}.git ${config.branch}`, {
             cwd: config.localRepo
-        }, (error) => {
+        }, error => {
             if (error) {
-                response.status(400).send();
+                response.status(400).end();
             }
             else {
-                response.status(200).send();
+                response.status(200).end();
             }
         });
         child.stdout.on("data", log);
         child.stderr.on("data", log);
     }
     else {
-        response.status(400).send();
+        response.status(400).end();
     }
 });
 
